cf1d547b2c2f10d6648bf2e6eb60d85dc471392f
annatakman <annatakman@gmail.com>
2020-05-24 11:38:19 +0200

    style login page

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  font-family: 'Josefin Sans', sans-serif;
+  background: #FcfAf1;
+  color:  #254b62;
+  border: 2px solid #cff532;
+  text-transform: uppercase;
+  cursor: pointer;
+  padding: 12px;
+  border-radius: 4px;
+  font-size: 24px;
+  display: block;
+  margin-top: 25px;
+`
+
+export const Button = ({ title, onClick }) => (
+  <StyledButton onClick={onClick}>{title}</StyledButton>
+)
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import { Button } from './Button'
 
 const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+    label {
+      margin: 10px;
+      color:  #254b62;
+    }
+    input {
+      width: 100%;
+    }
+`
+
+const Header = styled.h1`
+  color:  #254b62;
+  font-size: 40px;
+  margin-top: 40px;
 `
 
 export const Login = () => {
@@ -12,26 +34,25 @@ export const Login = () => {
   const url = 'https://anna-project-auth.herokuapp.com/sessions'
   // posts email and password to the api
 
-  const handelLogin = event => {
+  const handleLogin = event => {
     event.preventDefault()
     fetch('url', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' }
-    }).then(() => {
-      window.location.reload()
     })
-  } //Här ska det komma en .then av något slag 
-  //vi behöver local storage någonstans
+    //Här ska det komma en .then av något slag 
+    //vi behöver local storage någonstans
+  }
 
   return (
     <Section>
-      <form>
-        <label for='email'>Email </label>
-        <input type='text' id='email' name='email' />
-        <label for='password'>Password </label>
-        <input type='text' id='password' name='password' />
-      </form>
+      <Header> Welcome</Header>
+      <Form>
+        <label for='email'>Email  <input type='text' id='email' name='email' /> </label>
+        <label for='password'>Password  <input type='text' id='password' name='password' /> </label>
+      </Form>
+      <Button title='Login' onClick={handleLogin} />
     </Section>
   )
 }
